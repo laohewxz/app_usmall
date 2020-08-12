8503cff63e9cebe8630c35a14b0ab9c6364d32e6
laohewxz <1290155154@qq.com>
2020-08-12 11:08:14 +0800

    0812 懒加载

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import {Switch,Route,Redirect} from "react-router-dom"
+import asyncComponent from "./utils/asyncComponent"
+import MyRoute from "./pages/MyRoute/MyRoute"
 
-import Index from "./pages/Index/Index"
-import Fenlei from "./pages/Fenlei/Fenlei"
-import Detail from "./pages/Detail/Detail"
-import Login from "./pages/Login/Login"
-import Register from "./pages/Register/Register"
-import ClassGoods from "./pages/ClassGoods/ClassGoods"
+// import Index from "./pages/Index/Index"
+// import Detail from "./pages/Detail/Detail"
+// import Login from "./pages/Login/Login"
+// import Register from "./pages/Register/Register"
+// import ClassGoods from "./pages/ClassGoods/ClassGoods"
+
+//懒加载
+const Index = asyncComponent(()=>import("./pages/Index/Index"))
+const Detail = asyncComponent(()=>import("./pages/Detail/Detail"))
+const Login = asyncComponent(()=>import("./pages/Login/Login"))
+const Register = asyncComponent(()=>import("./pages/Register/Register"))
+const ClassGoods = asyncComponent(()=>import("./pages/ClassGoods/ClassGoods"))
+
+
+
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path="/index" component={Index}></Route>
-        <Route path="/fenlei" component={Fenlei}></Route>
-        <Route path="/detail" component={Detail}></Route>
         <Route path="/login" component={Login}></Route>
-        <Route path="/register" component={Register}></Route>
-        <Route path="/classgoods" component={ClassGoods}></Route>
+        <MyRoute path="/index" component={Index}></MyRoute>
+        <MyRoute path="/detail" component={Detail}></MyRoute>
+        <MyRoute path="/register" component={Register}></MyRoute>
+        <MyRoute path="/classgoods" component={ClassGoods}></MyRoute>
         <Redirect to="/login"></Redirect>
       </Switch>
     </div>
